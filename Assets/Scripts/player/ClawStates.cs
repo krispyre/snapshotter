4a8815e9684fd4d439ef90670329efab46267de2
kris <kristenccwong@gmail.com>
2026-08-19 17:10:05 +0800

    oop stuff

--- Assets/Scripts/player/ClawStates.cs
+++ Assets/Scripts/player/ClawStates.cs
@@ -21,29 +21,29 @@ public class ClawState : IState
 {
     protected readonly PlayerMovement p;
     public ClawState(PlayerMovement p) => this.p = p;
-    public void Enter() { }
-    public void Exit() { }
-    public void FixedUpdate() { }
+    public virtual void Enter() { }
+    public virtual void Exit() { }
+    public virtual void FixedUpdate() { }
 }
 public sealed class ClawReady : ClawState
 {
     //so copy pastey, any better options
     public ClawReady(PlayerMovement p) : base(p) { }
-    public void Enter() { }
+    public override void Enter() { }
 
-    public void FixedUpdate()
+    public override void FixedUpdate()
     {
         if (p.shootPressed)
             p.clawFsm.SetState(p.clawFsm.clawShooting);
     }
 
-    public void Exit() { }
+    public override void Exit() { }
 }
 
 public sealed class ClawShooting : ClawState
 {
     public ClawShooting(PlayerMovement p) : base(p) { }
-    public void Enter()
+    public override void Enter()
     {
         Vector3 aim = p.clawPointer.position - p.transform.position;
         Ray ray = new Ray(p.transform.position, aim);
@@ -73,7 +73,7 @@ public sealed class ClawShooting : ClawState
         p.state = PlayerMovement.PlayerState.Clawing;
     }
 
-    public void FixedUpdate()
+    public override void FixedUpdate()
     {
         p.claw.transform.rotation = p.LookAt(p.transform.position, p.landingTarget);
         Vector3 vel = p.LinearVel(p.transform.position, p.landingTarget, p.clawParams.flyTime);
@@ -87,7 +87,7 @@ public sealed class ClawShooting : ClawState
             p.clawFsm.SetState(p.missed ? p.clawFsm.clawMiss : p.clawFsm.clawGrabbing);
         }
     }
-    public void Exit() { }
+    public override void Exit() { }
 }
 
 public sealed class ClawMiss : ClawState
@@ -95,54 +95,54 @@ public sealed class ClawMiss : ClawState
     public ClawMiss(PlayerMovement p) : base(p) { }
     int wait;
 
-    public void Enter()
+    public override void Enter()
     {
         wait = p.clawParams.pullDelay;
         p.claw_xVel = 0;
         p.claw_yVel = 0;
     }
 
-    public void FixedUpdate()
+    public override void FixedUpdate()
     {
         if (wait > 0) { wait--; }
         else { p.clawFsm.SetState(p.clawFsm.clawReturn); }
     }
 
-    public void Exit() { }
+    public override void Exit() { }
 }
 
 public sealed class ClawGrabbing : ClawState
 {
     public ClawGrabbing(PlayerMovement p) : base(p) { }
-    public void Enter()
+    public override void Enter()
     {
         p.claw_xVel = 0;
         p.claw_yVel = 0;
     }
 
-    public void FixedUpdate()
+    public override void FixedUpdate()
     {
         p.claw.transform.rotation = p.LookAt(p.transform.position, p.landingTarget);
         if (p.shootPressed)
             p.clawFsm.SetState(p.clawFsm.clawReturn);
     }
 
-    public void Exit() { }
+    public override void Exit() { }
 }
 
 public sealed class ClawReturn : ClawState
 {
     public ClawReturn(PlayerMovement p) : base(p) { }
-    public void Enter() => ApplyFlight();
+    public override void Enter() => ApplyFlight();
 
-    public void FixedUpdate()
+    public override void FixedUpdate()
     {
         ApplyFlight();
         if (Vector3.Distance(p.claw.transform.position, p.transform.position) < 0.02f)
             p.clawFsm.SetState(p.clawFsm.clawReady);
     }
 
-    public void Exit()
+    public override void Exit()
     {
         p.claw.SetActive(false);
         p.state = p.prevState;
